5dc02226ee23aca4b9306009795cc937e22d854a
Daniel Podolsky <78098754+DanielPodolsky@users.noreply.github.com>
2024-08-31 19:10:11 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+import { SERVICE_ID, TEMPLATE_ID } from "./config.js";
+
+// NAVBAR
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLinks = navMenu.querySelectorAll("li");
+
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+});
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", closeMenu);
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", closeMenu);
+});
+
+const sendEmail = () => {
+  const serviceId = "service_19wg4v9";
+  const templateId = "template_b6ujb8o";
+
+  emailjs
+    .sendForm(serviceId, templateId, document.getElementById("contact-form"))
+    .then(alert("Your message has been sent :)"));
+};
